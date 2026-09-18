8babcf54aad36341aad583bb7281b50584288e5c
levkor <lev@fossko.ru>
2026-09-18 13:00:13 +0300

    New errors format

--- src/toolkit/validator.py
+++ src/toolkit/validator.py
@@ -1,15 +1,26 @@
 from .errors import *
+import sys
 
 # Если ошибка есть, пишет сообщение с указателем на ошибку
 def validation_calc(tokens):
     if operation_before_first_operand(tokens)[0]:
-            raise ValueError(f"Operation before the first operand: {operation_before_first_operand(tokens)[1]}")
+            print(f"Operation before the first operand: {operation_before_first_operand(tokens)[1]}", file=sys.stderr)
+            sys.exit(2)
+            #raise ValueError(f"Operation before the first operand: {operation_before_first_operand(tokens)[1]}")
     if sequential_operations(tokens)[0]:
-        raise ValueError(f"Sequential Operations: {sequential_operations(tokens)[1]}")
+        print(f"Sequential Operations: {sequential_operations(tokens)[1]}", file=sys.stderr)
+        sys.exit(2)
+        #raise ValueError(f"Sequential Operations: {sequential_operations(tokens)[1]}")
     if float_mistake(tokens)[0]:
-        raise ValueError(f"Float mistake: {float_mistake(tokens)[1]}")
+        print(f"Float mistake: {float_mistake(tokens)[1]}", file=sys.stderr)
+        sys.exit(2)
+        #raise ValueError(f"Float mistake: {float_mistake(tokens)[1]}")
     if no_operand_after_operation(tokens)[0]:
-        raise ValueError(f"No operand after operation: {no_operand_after_operation(tokens)[1]}")
+        print(f"No operand after operation: {no_operand_after_operation(tokens)[1]}", file=sys.stderr)
+        sys.exit(2)
+        #raise ValueError(f"No operand after operation: {no_operand_after_operation(tokens)[1]}")
     if division_by_zero(tokens)[0]:
-         raise ZeroDivisionError(f"Division by zero occurred: {"".join(division_by_zero(tokens)[1])}")
+         print(f"Division by zero occurred: {"".join(division_by_zero(tokens)[1])}", file=sys.stderr)
+         sys.exit(2)
+         #raise ZeroDivisionError(f"Division by zero occurred: {"".join(division_by_zero(tokens)[1])}")
     return True
