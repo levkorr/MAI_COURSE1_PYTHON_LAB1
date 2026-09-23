ff961094e9f7ee02bf9b222fda426fc2d88e933e
levkor <lev@fossko.ru>
2026-09-23 22:08:36 +0300

    argparse instead of sys

--- src/toolkit/__main__.py
+++ src/toolkit/__main__.py
@@ -1,43 +1,58 @@
 import sys
+import argparse
 from sys import argv
 from .json_dump import calc_dump, convert_dump
-from .tokenizer import tokenize, shunting_yard
 from .calculator import calculate
-from .validator import validation_calc, validation_convert, initial_validation_calc
+from .tokenizer import tokenize, shunting_yard
+from .validator import validation_convert, validation_calc, initial_validation_calc
 from .converter import convert
 
-# python -m toolkit calc "-136++25266++24.3-4*+4-+5--.6"
-# python -m toolkit convert 129 --from kg --to g
-# "-136++25266++24.3-4*+4-+5--.6"
+def main():
+    # Создаем парсер через argparse
+    parser = argparse.ArgumentParser(prog="toolkit")
+    commands = parser.add_subparsers(dest="command", required=True)
+
+    # Для команды calc
+    parse_calc = commands.add_parser("calc")
+    parse_calc.add_argument("expression")
+
+    # Для команды convert
+    parse_convert = commands.add_parser("convert")
+    parse_convert.add_argument("value")
+    parse_convert.add_argument("--from", dest="from_metric", required=True)
+    parse_convert.add_argument("--to", dest="to_metric", required=True)
+
+    args = parser.parse_args()
+
+    if args.command == "calc":  # Если калькуляция
+        expr = args.expression
+
+        initial_validation_calc(expr)  # Предварительная валидация выражения
+        tokenized_expression = tokenize(expr)  # Токенизация выражения
+        validation_calc(tokenized_expression)  # Валиадция токенизированного выражения
+        rpn_expression = shunting_yard(tokenized_expression)  # Обратная польская нотация токенизированного выражения
+        calculated_expression = calculate(rpn_expression)  # Итоговый подсчет
+
+        print(calculated_expression)
+        calc_dump(expr, calculated_expression)  # Выгрузка успешного запуска
+        sys.exit(0)  # Успешное завершение программы
+
+    elif args.command == "convert":
+        value = float(args.value)
+        from_metric = args.from_metric
+        to_metric =  args.to_metric
+
+        validation_convert(value, from_metric, to_metric)  # Валидация
+        converted_value = convert(value, from_metric, to_metric)  # Перевод величин
+
+        print(converted_value)  # Итоговый вывод
+        convert_dump(value, converted_value, from_metric, to_metric)  # Выгрузка успешного запуска
+        sys.exit(0)  # Успешное завершение программы
+
+
 
 if __name__ == "__main__":
     try:
-        #cli_validation(argv)
-        if argv[1] == "calc":  # Если калькуляция
-            expr = argv[2] # Выражение
-            initial_validation_calc(expr)  # Предварительная валидация выражения
-            tokenized_expression = tokenize(expr)  # Токенизация выражения
-            # Валиадция токенизированного выражения
-            validation_calc(tokenized_expression)
-            # Обратная польская нотация токенизированного выражения
-            rpn_expression = shunting_yard(tokenized_expression)
-            calculated_expression = calculate(rpn_expression)  # Итоговый подсчет
-
-            print(calculated_expression)  # Итоговый вывод
-            calc_dump(expr, calculated_expression)  # Выгрузка успешного запуска
-            sys.exit(0)  # Успешное завершение программы
-
-        elif argv[1] == "convert":  # Если конвертация
-            value = float(argv[2])  # Значение
-            from_metric = argv[4].lower()  # Из какой величины
-            to_metric = argv[6].lower()  # В какую величину
-
-            validation_convert(value, from_metric, to_metric)  # Валидация
-            converted_value = convert(value, from_metric, to_metric)  # Перевод величин
-
-            print(converted_value)  # Итоговый вывод
-            convert_dump(value, converted_value, from_metric, to_metric)  # Выгрузка успешного запуска
-            sys.exit(0)  # Успешное завершение программы
+        main()
     except Exception as e:
         print(e, file=sys.stderr)
-        sys.exit(2)
